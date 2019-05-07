8f32ef19bfb73e81b794b9ca553174c1b797c06b
thopkins <thomashopkins000@gmail.com>
2019-05-07 00:14:48 -0400

    started GUI

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from random import randint
+import tkinter as tk
+
+class Window(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+
+        self.prompt = tk.Label(self, text="Enter a number:", anchor="w")
+        self.entry = tk.Entry(self)
+        self.submit = tk.Button(self, text="Submit", command = self.calculate)
+        self.output = tk.Label(self, text="")
+
+        self.prompt.pack(side="top", fill="x")
+        self.entry.pack(side="top", fill="x", padx=20)
+        self.output.pack(side="top", fill="x", expand=True)
+        self.submit.pack(side="right")
+
+    def calculate(self):
+        try:
+            i = int(self.entry.get())
+            result = "%s*2=%s" % (i, i*2)
+        except ValueError:
+            result = "Please enter digits only"
+        self.output.configure(text=result)
 
 def right(board):
     for i in range(0, 4, 1):
@@ -69,6 +92,9 @@ def printBoard(board):
 
 
 def main():
+    root = tk.Tk()
+    Window(root).pack(fill="both", expand=True)
+    root.mainloop()
     board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
     for i in range(0,2,1):
         firstI = randint(0,3)
